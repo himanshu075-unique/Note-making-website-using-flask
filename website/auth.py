85a65d9e1c30cb3dcb7686dc2053e23b7e15d6b6
himanshu075-unique <himanshuchoudhary9365@gmail.com>
2026-02-04 00:48:19 +0530

    Flash using work

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
@@ -6,7 +6,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
 
-    return render_template("login.html", text="Testing", user="Himanshu", boolean=True)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -20,15 +20,15 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
     
-    if len(email) < 4:
-        flash('Email must be greater than 3 characters.', category='error')
-    elif len(firstName) < 2:
-        flash('First name must be greater than 1 characters.', category='error')
-    elif password1 != password2:
-        flash('Passwords don\'t match.', category='error')
-    elif len(password1) < 7:
-        flash('Password must be greater at least 7 characters.', category='error')
-    else:
-        flash('Account created!', category='success')
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
 
     return render_template("sign_up.html")
